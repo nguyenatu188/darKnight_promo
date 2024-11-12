f71c0da4cb6c5721fa6c5ed3317b24fc53cce8da
Natus <nnguyentu13@gmail.com>
2024-11-12 12:40:55 +0700

    du ma

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-import { demon, ghost, particles, fireSkull, review1, review2, review3 } from "../assets"
+import { world, story, combat, gameplay, demon, ghost, particles, fireSkull, review1, review2, review3 } from "../assets"
 
 export const navLinks = [
   {
@@ -23,22 +23,22 @@ export const keyFeatures = [
 {
   title: "THẾ GIỚI MỚI LẠ, RỘNG LỚN",
   desc: "Chào đón bạn vào Caladrias – một vùng đất bị nguyền rủa rộng lớn và đầy ắp những điều bí ẩn. Khám phá những khu rừng tăm tối, ngọn núi chìm trong bóng đêm, và những thành phố bị lãng quên, tất cả đều ẩn chứa những câu chuyện chưa được kể và thử thách khó khăn. Mỗi bước đi sẽ đưa bạn đến những vùng đất mới lạ, mở rộng hành trình của Dar knight trong cuộc chiến chống lại bóng tối.",
-  gif: demon
+  gif: world
 },
 {
   title: "CỐT TRUYỆN BÍ ẤN VÀ THÚ VỊ",
   desc: "Câu chuyện trong Dar knight đan xen giữa huyền thoại và thực tại, với những bí ẩn cần được khám phá và những lựa chọn quan trọng ảnh hưởng đến vận mệnh của thế giới. Câu chuyện không chỉ xoay quanh cuộc hành trình thu thập Mảnh Vỡ Tấm Màn mà còn tiết lộ những sự thật đau đớn về sự sụp đổ của ánh sáng và bóng tối. Mỗi bước đi của bạn sẽ dần mở ra những mảnh ghép mới, dẫn đến những bất ngờ và tiết lộ đầy thú vị.",
-  gif: ghost
+  gif: story
 },
 {
   title: "GAMEPLAY ĐA DẠNG, PHONG PHÚ",
   desc: "Trải nghiệm gameplay phong phú và đa dạng, nơi bạn không chỉ tham gia vào những trận chiến ác liệt với các quái vật kinh hoàng mà còn phải giải quyết những câu đố hóc búa, mở khóa những phần thưởng quý giá. Combat trong game vô cùng phong phú với nhiều loại quái vật và boss khổng lồ, mỗi trận đấu đều là một thử thách mới. Cùng với đó, hệ thống kỹ năng đa dạng cho phép bạn tùy biến phong cách chiến đấu, từ tấn công mạnh mẽ cho đến sử dụng bóng tối để đánh lừa kẻ thù.",
-  gif: fireSkull
+  gif: gameplay
 },
 {
   title: "HỆ THỐNG COMBAT CĂNG VL CĂNG",
   desc: "Hệ thống combat trong Dar knight được thiết kế cực kỳ kịch tính và căng thẳng. Bạn sẽ phải sử dụng mọi chiến thuật và kỹ năng để đối đầu với kẻ thù, từ những sinh vật méo mó bảo vệ ánh sáng cho đến các quái vật mạnh mẽ nhất. Mỗi trận chiến đều yêu cầu sự khéo léo và tính toán, từ việc chọn lựa vũ khí cho đến việc phối hợp các kỹ năng để chiến thắng. Đặc biệt, những cuộc đối đầu với boss sẽ là thử thách khó nhằn, yêu cầu bạn phải học hỏi từ thất bại để ngày càng mạnh mẽ hơn.",
-  gif: particles
+  gif: combat
 }
 ]
 
